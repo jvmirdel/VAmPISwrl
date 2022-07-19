92856c795bb3003cd9a4e545e140ea3defd19416
Jason Venner <jason@autonomiq.io>
2022-07-19 15:02:20 -0700

    set vulnerable to 0

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
           as it is a matter of bad practice. Such an example is the debug endpoint.
 '''
 vuln = int(os.getenv('vulnerable', 1))
-# vuln=1
+vuln=0
 # token alive for how many seconds?
 alive = int(os.getenv('tokentimetolive', 60))
 
